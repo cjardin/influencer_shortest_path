542940b41223aa5a7888add57798f5457ff169df
cjardin <cjardin@jardinlabs.com>
2021-11-26 11:42:37 -0800

    checkpoint

--- astar.py
+++ astar.py
@@ -48,6 +48,8 @@ def astar_search(graph, heuristics, start, end):
                 open.append(neighbor)
     # Return None, no path is found
     return None
+
+
 # Check if a neighbor should be added to open list
 def add_to_open(open, neighbor):
     for node in open:
